a00711ef18105043f928c3266eefd3d9151712e7
kkli08 <ke_2021@126.com>
2022-11-30 23:34:40 -0700

    done

--- sockets.py
+++ sockets.py
@@ -30,6 +30,7 @@ app.debug = True
 
 # https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/broadcaster.py
 
+
 class Client:
     def __init__(self):
         self.queue = queue.Queue()
@@ -41,11 +42,15 @@ class Client:
         return self.queue.get()
 
 # https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/broadcaster.py
+
+
 def send_all(msg):
     for client in clients:
         client.put(msg)
 
 # https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/broadcaster.py
+
+
 def send_all_json(obj):
     send_all(json.dumps(obj))
 
@@ -55,19 +60,19 @@ class World:
         self.clear()
         # we've got listeners now!
         self.listeners = list()
-        
+
     def add_set_listener(self, listener):
-        self.listeners.append( listener )
+        self.listeners.append(listener)
 
     def update(self, entity, key, value):
-        entry = self.space.get(entity,dict())
+        entry = self.space.get(entity, dict())
         entry[key] = value
         self.space[entity] = entry
-        self.update_listeners( entity )
+        self.update_listeners(entity)
 
     def set(self, entity, data):
         self.space[entity] = data
-        self.update_listeners( entity )
+        self.update_listeners(entity)
 
     def update_listeners(self, entity):
         '''update the set listeners'''
@@ -78,27 +83,33 @@ class World:
         self.space = dict()
 
     def get(self, entity):
-        return self.space.get(entity,dict())
-    
+        return self.space.get(entity, dict())
+
     def world(self):
         return self.space
 
-myWorld = World() 
+
+myWorld = World()
 clients = list()
 
-def set_listener( entity, data ):
-    ''' do something with the update ! '''
-    for client in clients:
-        client.put(flask.jsonify({entity: data}))
 
-myWorld.add_set_listener( set_listener )
-        
+def set_listener(entity, data):
+    ''' do something with the update ! '''
+    send_all_json({entity: data})
+    # for client in clients:
+    #     client.put(flask.jsonify({entity: data}))
+
+
+myWorld.add_set_listener(set_listener)
+
+
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
     return redirect("./static/index.html")
 
-def read_ws(ws,client):
+
+def read_ws(ws, client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
     # https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/broadcaster.py
@@ -149,30 +160,32 @@ def flask_post_json():
     else:
         return json.loads(request.form.keys()[0])
 
-@app.route("/entity/<entity>", methods=['POST','PUT'])
+
+@app.route("/entity/<entity>", methods=['POST', 'PUT'])
 def update(entity):
     '''update the entities via this interface'''
     post_json = flask_post_json()
     myWorld.set(entity, post_json)
     return flask.jsonify(myWorld.get(entity)), 200
 
-@app.route("/world", methods=['POST','GET'])    
+
+@app.route("/world", methods=['POST', 'GET'])
 def world():
     '''you should probably return the world here'''
     return flask.jsonify(myWorld.world()), 200
 
-@app.route("/entity/<entity>")    
+
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
     return flask.jsonify(myWorld.get(entity)), 200
 
 
-@app.route("/clear", methods=['POST','GET'])
+@app.route("/clear", methods=['POST', 'GET'])
 def clear():
     '''Clear the world out!'''
     myWorld.clear()
     return flask.jsonify(myWorld.world()), 200
-
 
 
 if __name__ == "__main__":
